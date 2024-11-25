bc878ac12e573b6414f2116dad48ca4b34e3c84b
ProboDwi <gghanip8@gmail.com>
2024-11-25 20:34:21 +0700

    crud users

---
+++ app/views/user/create.php
@@ -0,0 +1,13 @@
+<!-- app/views/user/create.php -->
+<h2>Tambah Pengguna Baru</h2>
+<form action="/user/store" method="POST">
+    <label for="nama">Nama:</label>
+    <input type="text" name="nama" id="nama" required>
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" required>
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password" required>
+    <label for="alamat">Alamat:</label>
+    <input type="text" name="alamat" id="alamat" required>
+    <button type="submit">Simpan</button>
+</form>
